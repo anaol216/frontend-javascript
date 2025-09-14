001f0c628f7ebc1d99424ab61a4a8a97154e0624
anaol216 <atinafuanaol@gmail.com>
2025-09-14 23:43:06 +0300

    initialize TypeScript task with Teacher interface and configuration files

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,16 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  fullTimeEmployee: boolean;
+  location: string;
+  yearsOfExperience?: number;
+  [key: string]: any;
+} 
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+console.log(teacher3);

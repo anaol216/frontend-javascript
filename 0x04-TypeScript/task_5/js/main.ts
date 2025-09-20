61d2f60467a07f068282daf936cd9fab6df9d282
anaol216 <atinafuanaol@gmail.com>
2025-09-20 18:43:18 +0300

    modified:   0x04-TypeScript/task_5/js/main.ts

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,14 +1,16 @@
 export interface MajorCredits {
   credits: number;
 }
+
 export interface MinorCredits {
   credits: number;
 }
 
-function sumMajorCredits(subjects: MajorCredits[]): number {
-  return subjects.reduce((total, subject) => total + subject.credits, 0);
+// Example using explicit subjects
+ function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): number {
+  return subject1.credits + subject2.credits;
 }
 
-function sumMinorCredits(subjects: MinorCredits[]): number {
-  return subjects.reduce((total, subject) => total + subject.credits, 0);
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): number {
+   return subject1.credits + subject2.credits;
 }
